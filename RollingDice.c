26eebb5607bfd86eb565236958782f21a91b9690
MickyYuki <mizutani.y8227@gmail.com>
2023-06-09 17:20:01 +0900

    Greetings added

--- RollingDice.c
+++ RollingDice.c
@@ -5,14 +5,20 @@
 int main(void){
 
   int d1, d2;
+  char str[100];
 
   srand(time(NULL));
 
   d1 = rand() % 6 + 1;
   d2 = rand() % 6 + 1;
+
+  printf("What is your name?\n");
+  scanf("%s", str);
+  printf("Hello, %s!\n", str);
   
   printf("Rolling dice...\n");
   printf("Die 1: %d\n", d1);
   printf("Die 2: %d\n", d2);
   printf("Total value: %d\n", d1 + d2);
+
 }
